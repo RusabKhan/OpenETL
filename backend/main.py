6bf855c2ff121c0c18e7c4f938ff578e941dfa1f
Rusab Khan <rusabkhan7@gmail.com>
2024-11-24 17:08:32 +0500

    moved migrations and removed unnecessary files

--- backend/main.py
+++ backend/main.py
@@ -11,18 +11,18 @@ from fastapi.responses import ORJSONResponse
 # Define the SQLAlchemy models
 # FastAPI app
 
-
-app = FastAPI(default_response_class=ORJSONResponse)
-
-app.include_router(db_router)
-app.include_router(connector_router)
-
-app.on_event("startup")
-def create_document_table():
+def __init__():
     DatabaseUtils(engine=os.getenv('OPENETL_DOCUMENT_ENGINE'),
               hostname=os.getenv('OPENETL_DOCUMENT_HOST'),
               port=os.getenv('OPENETL_DOCUMENT_PORT'),
               username=os.getenv('OPENETL_DOCUMENT_USER'),
               password=os.getenv('OPENETL_DOCUMENT_PASS'),
               database=os.getenv('OPENETL_DOCUMENT_DB')).create_document_table()
+
+app = FastAPI(default_response_class=ORJSONResponse, on_startup=[__init__])
+
+app.include_router(db_router)
+app.include_router(connector_router)
+
+
 
