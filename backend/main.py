087a9585484f3e6e9ae97cd0fe85eb22393190eb
Rusab Khan <rusabkhan7@gmail.com>
2024-12-11 20:42:53 +0500

    deprecated streamlit frontend

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,18 @@
 from fastapi import FastAPI
 import os
 import sys
-
+from fastapi.middleware.cors import CORSMiddleware
 from utils.database_utils import DatabaseUtils
 
 sys.path.append(os.environ['OPENETL_HOME'])
 from app.database import router as db_router
 from app.connector import router as connector_router
-from app.scheduler import router as celery_router
+from app.worker import router as celery_router
+from app.scheduler import router as scheduler_router
 from app.pipeline import router as pipeline_router
+from app.oauth import router as oauth_router
 from fastapi.responses import ORJSONResponse
-from utils.__migrations__.app import OpenETLDocument
+from utils.__migrations__.app import OpenETLDocument, OpenETLOAuthToken
 from utils.__migrations__.scheduler import OpenETLIntegrations, OpenETLIntegrationsRuntimes
 
 
@@ -24,13 +26,27 @@ def __init__():
     db_class.create_table_from_base(base=OpenETLDocument)
     db_class.create_table_from_base(base=OpenETLIntegrations)
     db_class.create_table_from_base(base=OpenETLIntegrationsRuntimes)
+    db_class.create_table_from_base(base=OpenETLOAuthToken)
 
 
 app = FastAPI(default_response_class=ORJSONResponse, on_startup=[__init__])
+
+origins = [
+    "*"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(db_router)
 app.include_router(connector_router)
 app.include_router(celery_router)
 app.include_router(pipeline_router)
+app.include_router(oauth_router)
+app.include_router(scheduler_router)
 
 
