a11dbd3463a5ea98a59ecf5ae063c0d53e947347
Rusab Khan <rusabkhan7@gmail.com>
2024-12-14 00:45:32 +0500

    renamed models folder and added connection update and delete API

--- backend/app/connector.py
+++ backend/app/connector.py
@@ -2,10 +2,12 @@ from fastapi import APIRouter, Body, HTTPException, Request
 import os
 import sys
 
+sys.path.append(os.environ['OPENETL_HOME'])
+
+from app.models.main import IntegrationBody
 from utils.connector_utils import get_installed_connectors
 from utils.enums import AuthType, ConnectionType
 
-sys.path.append(os.environ['OPENETL_HOME'])
 from utils.database_utils import DatabaseUtils, get_open_etl_document_connection_details
 import utils.connector_utils as con_utils
 
@@ -20,18 +22,22 @@ async def get_installed_connectors_api():
 
 
 @router.post("/test_connection")
-async def test_connection_api(request: Request, connector_name: str = Body(...), connector_type: str = Body(...), auth_type: str = Body(...), auth_params: dict = Body(...)):
+async def test_connection_api(request: Request, connector_name: str = Body(...), connector_type: str = Body(...),
+                              auth_type: str = Body(...), auth_params: dict = Body(...)):
     return con_utils.connector_test_connection(connector_name, ConnectionType(connector_type), auth_type, **auth_params)
 
 
 @router.post("/store_connection")
-async def store_connection_api(request: Request, connection_credentials: dict = Body(...), connection_name: str = Body(...), connection_type: str = Body(...), connector_name: str = Body(...), auth_type: str = Body(...)):
+async def store_connection_api(request: Request, connection_credentials: dict = Body(...),
+                               connection_name: str = Body(...), connection_type: str = Body(...),
+                               connector_name: str = Body(...), auth_type: str = Body(...)):
     vals = get_open_etl_document_connection_details()
     auth_type = AuthType(auth_type)
-    data = {"connection_credentials": connection_credentials, "connection_name": connection_name, "connection_type": connection_type, "auth_type": auth_type, "connector_name": connector_name}
+    data = {"connection_credentials": connection_credentials, "connection_name": connection_name,
+            "connection_type": connection_type, "auth_type": auth_type, "connector_name": connector_name}
     return DatabaseUtils(**vals).write_document(data)
-    
-    
+
+
 @router.get("/get_connector_auth_details/{connector_name}/{connector_type}")
 async def get_connector_auth_details_api(request: Request, connector_name: str, connector_type: str):
     connector_type = ConnectionType(connector_type)
@@ -40,7 +46,7 @@ async def get_connector_auth_details_api(request: Request, connector_name: str, 
 
 @router.get("/get_connector_image/{connector_name}/{connector_type}")
 async def get_connector_image_api(request: Request, connector_name: str, connector_type: str):
-    return con_utils.get_connector_image(connector_name,connector_type)
+    return con_utils.get_connector_image(connector_name, connector_type)
 
 
 @router.get("/get_connector_metadata/{connector_name}/{connector_type}")
@@ -50,7 +56,7 @@ async def get_connector_metadata_api(request: Request, connector_name: str, conn
 
 
 @router.post("/get_created_connections")
-async def created_connections_api(request: Request, connector_type: str = Body(...), connection_name: str = Body(None) ):
+async def created_connections_api(request: Request, connector_type: str = Body(...), connection_name: str = Body(None)):
     try:
         return con_utils.get_created_connections(connector_type, connection_name)
     except Exception as e:
@@ -66,5 +72,25 @@ async def connector_fetch_metadata_api(request: Request):
         auth_options = body.get("auth_options")
         return con_utils.fetch_metadata(connection=connector_name, connection_type=connector_type,
                                         auth_options=auth_options)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.post("/update_connection")
+async def update_connection_api(request: Request, document_id: int = Body(...), kwargs: dict = Body(...)):
+    try:
+        body = await request.json()
+        db = DatabaseUtils(**get_open_etl_document_connection_details())
+        return db.update_openetl_document(document_id=document_id, **kwargs)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.delete("/delete_connection")
+async def delete_connection_api(request: Request, document_details: IntegrationBody = Body(...)):
+    try:
+        body = await request.json()
+        db = DatabaseUtils(**get_open_etl_document_connection_details())
+        return db.delete_document(document_details)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
