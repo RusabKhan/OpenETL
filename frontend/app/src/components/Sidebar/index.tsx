4e6672eb618e3f458dc1ef7474c9041d7725ee0c
Muhammad Ahmed <shiekhahmed135@gmail.com>
2024-12-28 01:34:40 +0500

    logo added

--- frontend/app/src/components/Sidebar/index.tsx
+++ frontend/app/src/components/Sidebar/index.tsx
@@ -396,14 +396,14 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
         {/* <!-- SIDEBAR HEADER --> */}
         <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
           <Link href="/">
-            <h1 className="text-white">OpenETL</h1>
-            {/* <Image
+            {/* <h1 className="text-white">OpenETL</h1> */}
+            <Image
               width={176}
               height={32}
-              src={"/images/logo/logo.svg"}
+              src={"/images/logo/logo_white.png"}
               alt="Logo"
               priority
-            /> */}
+            />
           </Link>
 
           <button
