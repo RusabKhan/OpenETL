f39b03c6ef071a16fde68194b3f324ea9527fdd1
Muhammad Ahmed <shiekhahmed135@gmail.com>
2025-01-04 22:08:56 +0500

    sidebar update

--- frontend/app/src/components/Tables/ETLTable.tsx
+++ frontend/app/src/components/Tables/ETLTable.tsx
@@ -17,38 +17,6 @@ const ETLTable: React.FC<ETLTableInterface> = (params) => {
 
   return (
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-      <div className="pb-4">
-        <label
-          htmlFor="input-group-1"
-          className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Search
-        </label>
-        <div className="relative mb-4 md:w-[25%]">
-          <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-3.5">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-              />
-            </svg>
-          </div>
-          <input
-            type="text"
-            id="input-group-1"
-            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 ps-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-            placeholder="Search for integration"
-          />
-        </div>
-      </div>
       <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400 rtl:text-right">
         <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
           <tr>
