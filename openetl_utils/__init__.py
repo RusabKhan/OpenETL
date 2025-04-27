c524aaedf5f55b3ed42494d426d6687309dfacfb
RusabKhan <rusabkhan7@gmail.com>
2025-04-27 06:18:09 +0500

    added version check

--- openetl_utils/__init__.py
+++ openetl_utils/__init__.py
@@ -7,15 +7,20 @@ from .enums import *
 from .main_api_class import *
 from .cache import *
 from .celery_utils import *
+import tomllib
+import pathlib
 
+__version__ = tomllib.loads(
+    pathlib.Path(__file__).parent.parent.joinpath("pyproject.toml").read_text()
+)["tool"]["poetry"]["version"]
 
 def _print_logo():
-    print("""
+    print(f"""
  ██████  ██████  ███████ ███    ██ ███████ ████████ ██      
 ██    ██ ██   ██ ██      ████   ██ ██         ██    ██      
 ██    ██ ██████  █████   ██ ██  ██ █████      ██    ██      
 ██    ██ ██      ██      ██  ██ ██ ██         ██    ██      
- ██████  ██      ███████ ██   ████ ███████    ██    ███████ 
+ ██████  ██      ███████ ██   ████ ███████    ██    ███████  {__version__}
                                                             
 """)
     print("Authored by DataOmni Solutions")
