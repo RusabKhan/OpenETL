3e26b135d101b5b59c9bae07959f8bd3f0e08cbd
Rusab Khan <74367699+RusabKhan@users.noreply.github.com>
2025-06-15 01:43:21 +0500

    Feature/spark version update (#129)
    * test for spark cluster mode
    * Spark version update
    * Batch ID fix

--- openetl_utils/spark_utils.py
+++ openetl_utils/spark_utils.py
@@ -94,7 +94,7 @@ class SparkConnection():
             # Build SparkSession
             spark_session = SparkSession.builder \
                 .master(os.getenv("SPARK_MASTER" , "spark://spark-master:7077")) \
-                .config("spark.driver.host", os.getenv("SPARK_DRIVER_HOST" , "openetl-pro-celery-worker")) \
+                .config("spark.driver.host", os.getenv("SPARK_DRIVER_HOST" , "openetl-celery-worker-1")) \
                 .config(conf=spark_conf) \
                 .getOrCreate()
 
